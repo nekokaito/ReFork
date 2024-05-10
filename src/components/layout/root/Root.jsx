2c17b58f64fb405ac01437cad8c768496c1c4a20
Siddiq Sazzad <siddiqsazzad001@gmail.com>
2024-05-10 22:06:07 +0600

    Footer Done

--- src/components/layout/root/Root.jsx
+++ src/components/layout/root/Root.jsx
@@ -2,15 +2,16 @@ import { Outlet } from "react-router-dom";
 import NavBar from "../navbar/NavBar";
 import Footer from "../footer/Footer";
 
-
 const Root = () => {
     return (
-        <div>
-            <NavBar></NavBar>
-            <Outlet></Outlet>
-            <Footer></Footer>
+        <div className="flex flex-col min-h-screen">
+            <NavBar />
+            <div className="flex-1">
+                <Outlet />
+            </div>
+            <Footer />
         </div>
     );
 };
 
-export default Root;
+export default Root; 
