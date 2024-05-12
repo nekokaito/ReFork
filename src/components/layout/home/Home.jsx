39ff34d421824f31d3d142c0657d0e45b4cfcc4f
Siddiq Sazzad <siddiqsazzad001@gmail.com>
2024-05-12 10:40:25 +0600

    Add Food Form also Lottie

--- src/components/layout/home/Home.jsx
+++ src/components/layout/home/Home.jsx
@@ -1,9 +1,11 @@
 import Header from "../../tools/header/Header";
+import Support from "./Support";
 
 const Home = () => {
     return (
         <div>
             <Header></Header>
+            <Support></Support>
         </div>
     );
 };
--- src/components/layout/home/Support.jsx
+++ src/components/layout/home/Support.jsx
@@ -1,9 +1,17 @@
+import Lottie from "lottie-react";
+import flower from "./flower.json"
 
 
 const Support = () => {
     return (
-        <section>
-            <h1></h1>
+        <section className=" flex flex-col md:flex-row justify-evenly items-center">
+            <div className=" w-96">
+                <Lottie animationData={flower} loop={true} />
+            </div>
+            <div className="w-1/2">
+                <h1 className="text-6xl font-jaro">Sharing is Caring</h1>
+                <p className="font-roboto p-2">We're excited to announce that through our website, we've been able to provide support to food aid initiatives in <b>Palestine</b>. Collaborating with local partners, we're making a real difference in addressing food insecurity in the region. </p>
+            </div>
         </section>
     );
 };
